f5ce2d6e6429d5ebd9c7d3d510f6f106648be0a4
ghost1497 <rfrankie04@gmail.com>
2018-02-06 15:45:38 -0500

    New Commit

---
+++ src/main/java/com/zipcodewilmington/mainApplication.java
@@ -0,0 +1,18 @@
+package com.zipcodewilmington;
+
+public class mainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+
+
+        int[] y = ;
+
+        for(int i = 0; i > 10; i++ ){
+            y[i] = i;
+        }
+        for (int j = 0; j > y.length; j++) {
+            System.out.println(y[j]);
+        }
+    }
+}
