6cc12c79e5d2472500912d2d5584f383fdf8bc5e
ghost1497 <rfrankie04@gmail.com>
2018-02-06 16:43:50 -0500

    Update mainApplication.java

--- src/main/java/com/zipcodewilmington/mainApplication.java
+++ src/main/java/com/zipcodewilmington/mainApplication.java
@@ -3,5 +3,6 @@ package com.zipcodewilmington;
 public class mainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Sike");
     }
 }
